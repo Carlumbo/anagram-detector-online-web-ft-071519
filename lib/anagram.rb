a0f8cd6851f81c9235726c1506e72aa5fa3e8154
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-30 16:00:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,18 +1,14 @@
-# Your code goes here!
-class Anagram 
-  attr_accessor :word 
+class Anagram
+  attr_accessor :w
   
-  def initialize(word)
-    @word = word 
-  end 
-  
-  def match(find_array)
-    find_array.find_all do |array|
-      if word.split("").sort == array.split("").sort
-        word
+  def initialize(w)
+    @w = w
+    
+    def match(find_array)
+      find_array.find_all do |a|
+        if w.split("").sort == a.split("").sort
+          w
+        end 
       end 
     end 
   end 
-end 
-        
-      
